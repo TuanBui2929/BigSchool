62d46ecaf316cd03b5e06769743285dd68158af3
TuanBui2929 <76864341+TuanBui2929@users.noreply.github.com>
2021-07-09 20:14:32 +0700

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using BigSchool.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BigSchool.Controllers
+{
+  
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            //List<ListComming> list = new List<ListComming>();
+            //BigSchoolContext bigSchoolContext = new BigSchoolContext();
+            //var upcomming = bigSchoolContext.Courses.Where(p => p.DateTime > DateTime.Now).OrderBy(m => m.DateTime).ToList();
+            //foreach (Course i in upcomming)
+            //{
+            //    ApplicationUser user = System.Web.HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>().FindById(i.LecturerId);
+            //    list.Add(new ListComming(user.Name, i.DateTime));
+            //}
+            //return View(list);
+            BigSchoolContext bigSchoolContext = new BigSchoolContext();
+            var upcomming = bigSchoolContext.Courses.Where(p => p.DateTime > DateTime.Now).OrderBy(m => m.DateTime).ToList();
+            foreach (Course i in upcomming)
+            {
+                ApplicationUser user = System.Web.HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>().FindById(i.LecturerId);
+                i.Name = user.Name;
+            }
+            return View(upcomming);
+        }
+
+
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
